5fbd2426bedc0b4874ec800537c2ab67bf6a724a
kostyaalehin <koss595959@gmail.com>
2022-04-22 23:34:01 +0500

    Lesson 2. Work with Basic Operators

---
+++ SwiftLesson02.playground/Contents.swift
@@ -0,0 +1,46 @@
+
+let bigNumber = 19_280_198_201_982
+let oct = 0o12
+let number: UInt8 = 255
+let hex     = 0xff
+let binary  = 0b11111111
+
+Int.max
+Int8.max
+Int8.min
+UInt8.max
+UInt8.min
+
+13.23423e2
+13.23423e-2
+
+// для 16-й системы используется символ p (2 в степени)
+0x5p2
+0x5p-2
+
+/*
+//let a = 5
+//let b = 5.0
+//let c = a + Int(b)
+
+let a = 5
+let b = 5.5
+let c = Double(a) + b
+*/
+
+//let c = 5 + 5.2
+
+/*
+typealias newNumber = Int
+let a = 5
+let b = 5.4
+let c = a + newNumber(b)
+
+let d = false
+if d {
+    print("Hi!")
+} else {
+    print("Bye!")
+}
+*/
+
