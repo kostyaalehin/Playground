3bb3b9562e9da49a8af2584a6f65f0220f26a067
kostyaalehin <koss595959@gmail.com>
2022-04-22 22:27:03 +0500

    Lesson 1. The Basic in Swift

---
+++ SwiftLesson01.playground/Contents.swift
@@ -0,0 +1,23 @@
+
+
+let redColor = "red"
+let greenColor = "green"
+let blueColor = "blue"
+
+var color: String
+
+color =  redColor
+color
+color = greenColor
+color
+color = blueColor
+print(color)
+print(blueColor)
+print("Red color value = \(redColor)\nGreen color value = \(greenColor)")
+
+// Home Work
+
+let student1 = "Alexey Slow"
+let student2 = "Oleg Run"
+let student3 = "Natash Four"
+print ("In the our university study \(student1) and \(student2) and \(student3)" )
