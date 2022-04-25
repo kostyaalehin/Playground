b1fda205bd2864b5bb24c991c1443c1e7da506ba
kostyaalehin <koss595959@gmail.com>
2022-04-25 17:11:26 +0500

    SwiftLesson04. Optionals

--- SwiftLesson04.playground/Contents.swift
+++ SwiftLesson04.playground/Contents.swift
@@ -1,1 +1,43 @@
 
+// продажи
+/*
+50 + 20 = 70
+10 - 10 = 0
+*/
+//server requrst
+/*
+is it Saturday?
+
+true / false / nil
+*/
+
+var apples: Int? = 5
+
+//apples = nil
+
+if apples == nil {
+    print("nil apples")
+} else {
+    //print(apples)
+    
+    _ = apples! + 2
+}
+
+if var number = apples {
+    number = number + 2
+} else {
+    print("nil apples")
+}
+
+let age = "20"
+if Int(age) != nil {
+    let ageNumber = Int(age);
+    print (ageNumber!)
+}
+
+var apples2: Int! = nil
+apples2 = 2
+
+assert(apples2 != nil, "oh no!!!")
+
+apples2 = apples2 + 2
