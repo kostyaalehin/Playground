1add2080dc8a97b485fd447711a6f3366ee07fec
kostyaalehin <koss595959@gmail.com>
2022-04-25 18:21:37 +0500

    SwiftLesson05. Work with Basic Operators

---
+++ SwiftLesson05.playground/Contents.swift
@@ -0,0 +1,48 @@
+import Darwin
+
+let total = 5 + 6 - 8 * 3 - 5 / 10
+
+let div = 10 / 3
+let rest = 13 % 3
+
+var small: UInt8 = 0xff
+
+//переполнение
+small = small &+ 5
+
+let str = "Hi " + "sd"
+
+/*
+if a < b {
+    c = a
+} else {
+    c = b
+}
+
+c = a < b ? a : b
+*/
+var c: Int
+let text = "123"
+let n = Int(text)
+/*
+if n != nil {
+    c = n!
+} else {
+    c = 0
+}
+*/
+
+/*
+if let opt = n {
+    c = opt
+} else {
+    c = 0
+}
+*/
+
+c = n ?? 0
+
+var sum = 5
+sum = sum + 1
+sum += 1
+
