9db104cb88e425107242e630a7aef1342e87236d
kostyaalehin <koss595959@gmail.com>
2022-04-26 22:06:27 +0500

    SwiftLesson06. Strings

---
+++ SwiftLessons06.playground/Contents.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+var str = ""
+
+str = str + "a"
+str += "b"
+
+var str1 = "a"
+var str2 = str1
+
+str1 = "b"
+
+str1
+str2
+
+str.isEmpty
+
+let char1 : Character = "x"
+for c in "Hello" {
+    print(c)
+}
+
+str1.append(char1)
+
+//(str1 as NSString).length
+//NSString(string: str1).length
+
+str1.count
