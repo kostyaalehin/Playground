f1853412e206c2b266ada2a217a9a9cd33cf953c
kostyaalehin <koss595959@gmail.com>
2022-05-12 19:29:52 +0500

    SwiftLesson20. ARC (Automatic Reference Counting)

--- SwiftLesson20/SwiftLesson20/main.swift
+++ SwiftLesson20/SwiftLesson20/main.swift
@@ -1,11 +1,92 @@
-//
-//  main.swift
-//  SwiftLesson20
-//
-//  Created by Константин Алехин on 12.05.2022.
-//
+var playground = true
 
-import Foundation
+class Student {
+    
+    unowned var teacher : Teacher
+    
+    deinit {
+        print("goodbye student")
+    }
+    
+    init(teacher : Teacher) {
+        self.teacher = teacher
+    }
+}
 
-print("Hello, World!")
+class Teacher {
+    
+    var student : Student!
+    
+    var test : (() -> ())?
+    
+    lazy var test2 : (Bool) -> () = {[unowned self] (a: Bool) in
+//        print(self)
+        print(self.student)
+    }
+    
+    init() {
+        self.student = Student(teacher: self)
+    }
+    
+    deinit {
+        print("goodbye teacher")
+    }
+}
 
+var closure : (() -> ())?
+
+if playground {
+    
+//    var t: Teacher?
+    
+    var teacher = Teacher()
+    
+    teacher.test2(true)
+    
+    teacher.test = {
+        [unowned teacher] in
+        print(teacher)
+    }
+    
+//    if playground {
+//        var student = Student(teacher: teacher)
+//        teacher.student = student
+//    }
+    
+//    closure = {
+//        print(teacher)
+//    }
+    
+    print("exit playground")
+}
+
+class Human {
+    var name = "a"
+}
+
+var h = Human()
+
+var x = 10
+var y = 20
+
+var closure3 : () -> () = {
+    [x, y] in
+    print("\(x) \(y)")
+}
+
+var closure2 : (Int) -> Int = {[x, y, h] (a:Int) -> Int in
+    
+    print("\(x) \(y) \(h.name)")
+    return a
+}
+
+closure2(1)
+
+x = 30
+y = 40
+h = Human()
+h.name = "b"
+
+closure2(1)
+
+print("end")
